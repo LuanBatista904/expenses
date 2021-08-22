957ee6dc84a0c827ef87d4c52269347f03012c64
luanbatista904 <luabatista904@outlook.com>
2021-08-22 17:16:18 -0400

    atualização

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -46,62 +46,72 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextField(
-              controller: titleController,
-              onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
-                labelText: 'Título',
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: titleController,
+                onSubmitted: (_) => _submitForm(),
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
               ),
-            ),
-            TextField(
-              controller: valueController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
-                labelText: 'Valor (R\$)',
+              TextField(
+                controller: valueController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitForm(),
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)',
+                ),
               ),
-            ),
-            Container(
-              height: 70,
-              child: Row(
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    // ignore: unnecessary_null_comparison
+                    Expanded(child: Text(_selectedDate == null ? 'Nenhuma data selecionada!': 'Data Selecionada: ${DateFormat('d/M/y').format(_selectedDate)}')),
+                    TextButton(
+                      child: Text(
+                        'Selecionar Data',
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _showDatePicker,
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  // ignore: unnecessary_null_comparison
-                  Expanded(child: Text(_selectedDate == null ? 'Nenhuma data selecionada!': 'Data Selecionada: ${DateFormat('d/M/y').format(_selectedDate)}')),
-                  TextButton(
-                    child: Text(
-                      'Selecionar Data',
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: _showDatePicker,
-                  )
+                  ElevatedButton(
+                      child: Text(
+                        'Nova Transação',
+                        style: TextStyle(
+                            color: Colors.white,
+                            backgroundColor: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _submitForm),
                 ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                    child: Text(
-                      'Nova Transação',
-                      style: TextStyle(
-                          color: Colors.white,
-                          backgroundColor: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: _submitForm),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
+    )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'dart:math';
 import 'package:expenses/models/transactions.dart';
 import './components/transaction_list.dart';
@@ -42,6 +43,8 @@ class MyhomePage extends StatefulWidget {
 class _MyhomePageState extends State<MyhomePage> {
   final List<Transaction> _transactions = [];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
       return tr.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
@@ -53,7 +56,7 @@ class _MyhomePageState extends State<MyhomePage> {
         id: Random().nextDouble().toString(),
         title: title,
         value: value,
-        date: DateTime.now());
+        date: date);
 
     setState(() {
       _transactions.add(newTransaction);
@@ -79,27 +82,68 @@ class _MyhomePageState extends State<MyhomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Despesas Pessoais',
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _opentransactionFormModal(context),
-          )
-        ],
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandScape =
+        mediaQuery.orientation == Orientation.landscape;
+
+    final appBar = AppBar(
+      title: Text(
+        'Despesas Pessoais',
       ),
+      actions: [
+        if (isLandScape)
+          IconButton(
+            icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+            onPressed: () {
+              setState(() {
+                _showChart = !_showChart;
+              });
+            },
+          ),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _opentransactionFormModal(context),
+        ),
+      ],
+    );
+    final availableHeight = mediaQuery.size.height -
+        appBar.preferredSize.height -
+        mediaQuery.padding.top;
+
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Chart(_recentTransactions),
-              TransactionList(_transactions, _removeTransaction),
+              // if (isLandScape)
+              //   Row(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Text('Exibir Gráfico'),
+              //       Switch.adaptive(
+              //         activeColor: Theme.of(context).accentColor,
+              //           value: _showChart,
+              //           onChanged: (value) {
+              //             setState(() {
+              //               _showChart = value;
+              //             });
+              //           }),
+              //     ],
+              //   ),
+              if (_showChart || !isLandScape)
+                Container(
+                    height: availableHeight * (isLandScape ? 0.8 : 0.27),
+                    child: Chart(_recentTransactions)),
+              if (!_showChart || !isLandScape)
+                Container(
+                    height: availableHeight * (isLandScape ? 1 : 0.7),
+                    child: TransactionList(_transactions, _removeTransaction)),
             ]),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: Platform.isIOS 
+      ? Container() 
+      : FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _opentransactionFormModal(context),
       ),
